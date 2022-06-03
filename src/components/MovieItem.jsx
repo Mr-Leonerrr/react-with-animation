cfbd3e393e8e80c6c231b6401b6ecf37fe0da7fc
Leonardo Natera <lnatera27@gmail.com>
2022-06-02 22:41:40 -0500

    Fix: Updating movie structure

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -3,11 +3,12 @@ import React from "react";
 const MovieItem = ({ movie }) => {
     return (
         <div className="card animate__animated animate__fadeIn movie-item">
-            <img src={movie.Poster} alt={movie.Title} />
+            <img src={movie.image} alt={movie.title} />
             <div className="movie-item-info">
-                <h2>{movie.Title}</h2>
-                <p>{movie.Genre}</p>
-                <p>{movie.Year}</p>
+                <h2>{movie.title}</h2>
+                <p>{movie.description}</p>
+                <p>{movie.genres}</p>
+                <p>{movie.year}</p>
             </div>
         </div>
     );
--- src/helpers/getMovies.jsx
+++ src/helpers/getMovies.jsx
@@ -2,8 +2,22 @@ const API_URL = 'http://www.omdbapi.com/';
 const API_KEY = 'edc19b18';
 
 export const getMovies = async (searchValue) => {
-    const response = await fetch(`${API_URL}?t=${searchValue}&apikey=${API_KEY}`);
+    const url = `${API_URL}?t=${searchValue}&apikey=${API_KEY}`;
+    const response = await fetch(url);
     const data = await response.json();
     console.log(data);
-    return data;
+
+    if (data.Response === 'True') {
+        const movieData = {
+            id: data.imdbID,
+            title: data.Title,
+            year: data.Year,
+            image: data.Poster,
+            description: data.Plot,
+            genres: data.Genre.split(', ')
+        };
+        return movieData;
+    }
+
+    throw new Error(data.Error);
 }
