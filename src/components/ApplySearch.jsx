910b3990758245885924be7fc8cfe97a92086c50
Leonardo Natera <lnatera27@gmail.com>
2022-06-03 22:35:31 -0500

    Feat: Updating styles for search form

--- src/components/ApplySearch.jsx
+++ src/components/ApplySearch.jsx
@@ -18,9 +18,19 @@ const ApplySearch = ({ searchValue }) => {
 
     return (
         <>
-            <form onSubmit={handleSubmit}>
-                <input type="text" value={search} onChange={handleChange} />
-                <button>Search</button>
+            <form className="flex items-center" onSubmit={handleSubmit}>
+                <input
+                    className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                    type="text"
+                    placeholder="Search for a movie"
+                    value={search}
+                    onChange={handleChange}
+                />
+                <button type="button"
+                    className="ml-2 bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                >
+                    Search
+                </button>
             </form>
         </>
     );
